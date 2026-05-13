c814c9d8adab7a82da192d283f6838f0b4c70437
PedroMeneses112x <pedroms112x@gmail.com>
2026-05-13 11:36:55 -0400

    Se implementa Swagger y validaciones en pacientes

--- src/main/java/com/rednorte/ms_pacientes/controller/PacienteController.java
+++ src/main/java/com/rednorte/ms_pacientes/controller/PacienteController.java
@@ -3,6 +3,8 @@ package com.rednorte.ms_pacientes.controller;
 import com.rednorte.ms_pacientes.dto.PacienteRequest;
 import com.rednorte.ms_pacientes.dto.PacienteResponse;
 import com.rednorte.ms_pacientes.service.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,30 +20,41 @@ public class PacienteController {
     }
 
     @PostMapping
-    public PacienteResponse crearPaciente(@RequestBody PacienteRequest request) {
-        return service.crearPaciente(request);
+    public ResponseEntity<PacienteResponse> crearPaciente(
+            @Valid @RequestBody PacienteRequest request) {
+
+        return ResponseEntity.ok(service.crearPaciente(request));
     }
 
     @GetMapping
-    public List<PacienteResponse> obtenerPacientes() {
-        return service.obtenerPacientes();
+    public ResponseEntity<List<PacienteResponse>> obtenerPacientes() {
+
+        return ResponseEntity.ok(service.obtenerPacientes());
     }
 
     @GetMapping("/{id}")
-    public PacienteResponse obtenerPaciente(@PathVariable Long id) {
-        return service.obtenerPacientePorId(id);
+    public ResponseEntity<PacienteResponse> obtenerPaciente(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(service.obtenerPacientePorId(id));
     }
 
     @PutMapping("/{id}")
-    public PacienteResponse actualizarPaciente(
+    public ResponseEntity<PacienteResponse> actualizarPaciente(
             @PathVariable Long id,
-            @RequestBody PacienteRequest request) {
+            @Valid @RequestBody PacienteRequest request) {
 
-        return service.actualizarPaciente(id, request);
+        return ResponseEntity.ok(
+                service.actualizarPaciente(id, request)
+        );
     }
 
     @DeleteMapping("/{id}")
-    public void eliminarPaciente(@PathVariable Long id) {
+    public ResponseEntity<Void> eliminarPaciente(
+            @PathVariable Long id) {
+
         service.eliminarPaciente(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
